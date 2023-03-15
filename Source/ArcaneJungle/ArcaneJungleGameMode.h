ef8ef461dcbd19014c4e4fdd684fc809101ac9aa
Rin <zap504@amerike.edu.mx>
2023-03-15 14:36:20 -0600

    First commit

---
+++ Source/ArcaneJungle/ArcaneJungleGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ArcaneJungleGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AArcaneJungleGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AArcaneJungleGameMode();
+};
+
+
+
